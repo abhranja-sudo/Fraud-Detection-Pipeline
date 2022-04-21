53b694397fccfdfce628596e54f2d4b0ff656e47
ranjan <ar50645@gmail.com>
2022-04-20 23:25:04 -0700

    updated topic name for producer

--- producer.py
+++ producer.py
@@ -10,6 +10,7 @@ import threading
 KAFKA_HOST = '0.0.0.0:29092'
 conf = {'bootstrap.servers': KAFKA_HOST,
         'client.id': socket.gethostname()}
+TOPIC_TRANSACTIONS = 'transactions'
 
 def acked(err, msg):
     if err is not None:
@@ -23,7 +24,7 @@ def start_producing(messages):
         message_id = str(uuid.uuid4())
         message = {'request_id': message_id, 'data': json.loads(messages[i])}
 
-        producer.produce('events', json.dumps(message).encode('utf-8'), callback=acked)
+        producer.produce(TOPIC_TRANSACTIONS, json.dumps(message).encode('utf-8'), callback=acked)
         producer.flush()
 
         print("\033[1;31;40m -- PRODUCER: Sent message with id {}".format(message_id))
