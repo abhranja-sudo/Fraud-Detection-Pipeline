11fc528f507b69d413fe46bc1a12d89a84f90213
ranjan <ar50645@gmail.com>
2022-04-20 22:56:03 -0700

    Error logging

--- producer.py
+++ producer.py
@@ -14,14 +14,19 @@ KAFKA_HOST = '0.0.0.0:29092'
 conf = {'bootstrap.servers': KAFKA_HOST,
         'client.id': socket.gethostname()}
 
+def acked(err, msg):
+    if err is not None:
+        print("Failed to deliver message: %s: %s" % (str(msg), str(err)))
+    else:
+        print("Message produced: %s" % (str(msg)))
 
 def start_producing(messages):
-	producer =Producer(conf)
+	producer = Producer(conf)
 	for i in range(200):
 		message_id = str(uuid.uuid4())
 		message = {'request_id': message_id, 'data': json.loads(messages[i])}
 
-		producer.produce('events', json.dumps(message).encode('utf-8'))
+		producer.produce('events', json.dumps(message).encode('utf-8'), callback=acked)
 		producer.flush()
 
 		print("\033[1;31;40m -- PRODUCER: Sent message with id {}".format(message_id))
