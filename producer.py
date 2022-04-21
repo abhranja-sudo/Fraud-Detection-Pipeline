2c2449683d420eb02c0afbb8ebbe8abe1af31148
ranjan <ar50645@gmail.com>
2022-04-20 23:03:42 -0700

    producer cleanup

--- producer.py
+++ producer.py
@@ -1,13 +1,10 @@
 import joblib
-# from kafka import KafkaProducer, KafkaConsumer
 from confluent_kafka import Producer
 import socket
 
 import uuid
 import json
 import threading
-
-from time import sleep
 
 
 KAFKA_HOST = '0.0.0.0:29092'
@@ -21,16 +18,17 @@ def acked(err, msg):
         print("Message produced: %s" % (str(msg)))
 
 def start_producing(messages):
-	producer = Producer(conf)
-	for i in range(200):
-		message_id = str(uuid.uuid4())
-		message = {'request_id': message_id, 'data': json.loads(messages[i])}
+    producer = Producer(conf)
+    for i in range(200):
+        message_id = str(uuid.uuid4())
+        message = {'request_id': message_id, 'data': json.loads(messages[i])}
 
-		producer.produce('events', json.dumps(message).encode('utf-8'), callback=acked)
-		producer.flush()
+        producer.produce('events', json.dumps(message).encode('utf-8'), callback=acked)
+        producer.flush()
 
-		print("\033[1;31;40m -- PRODUCER: Sent message with id {}".format(message_id))
-		sleep(2)
+        print("\033[1;31;40m -- PRODUCER: Sent message with id {}".format(message_id))
+        producer.poll(3)
+
 
 if __name__ == '__main__':
     # Load the testing set from the file
@@ -41,5 +39,3 @@ if __name__ == '__main__':
     t = threading.Thread(target=start_producing(messages))
     threads.append(t)
     t.start()
-    # start_producing(messages)
-    # print(messages)
